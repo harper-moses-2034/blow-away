3e6500329cbef61c69ecfc5336135d02d79f2ab1
harper-moses-2034 <harper-moses-2034@solon.k12.ia.us>
2026-04-17 08:59:59 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ basic.showIcon(IconNames.Ghost)
 basic.forever(function () {
     for (let index = 0; index < 4; index++) {
         if (input.soundLevel() > 128) {
-            row = randint(10, 4)
+            row = randint(0, 4)
         }
-        col = randint(10, 4)
+        col = randint(0, 4)
     }
     if (led.point(col, row)) {
         led.unplot(col, row)
